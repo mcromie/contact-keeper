8dceae357c7b067f8a7a4cf6330dc107573813b5
Michaela Cromie <michaelacromie@lt-fljr6s2.corp.local>
2020-06-24 16:51:18 +0100

    Contact update and delete routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -53,15 +53,56 @@ router.post('/', [auth, [
 // @route PUT api/contacts/:id
 // @desc Update a contact
 // @access Private
-router.put('/:id', (req, res) => {
-    res.send('Update a contact');
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    //build contact object
+    const contactFields = {};
+    if(name) contactFields.name= name;
+    if(email) contactFields.email= email;
+    if(phone) contactFields.phone= phone;
+    if(type) contactFields.type= type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({ msg: 'contacts not found'});
+
+        //make sure user owns cotnact
+        if(contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'not authorized'});
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id, { $set: contactFields }, { new: true});
+
+        res.json(contact)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route DELETE api/contacts/:id
 // @desc Delete a contact
 // @access Private
-router.delete('/:id', (req, res) => {
-    res.send('Delete a contact');
-});
+router.delete('/:id', auth, async (req, res) => {
+    try {
+      let contact = await Contact.findById(req.params.id);
+  
+      if (!contact) return res.status(404).json({msg: 'Contact not found'});
+  
+      // Make sure user owns contact
+      if (contact.user.toString() !== req.user.id) {
+        return res.status(401).json({msg: 'Not authorized'});
+      }
+  
+      await Contact.findByIdAndRemove(req.params.id);
+  
+      res.json({msg: 'Contact removed'});
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
 module.exports = router;
